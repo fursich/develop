f5473e97deb720f7cde87e746fc61079632076a8
Koji Onishi <fursich0@gmail.com>
2017-04-22 16:45:51 +0900

    ビューコメント削除 リンク切れや写真修正

--- app/controllers/potepan_controller.rb
+++ app/controllers/potepan_controller.rb
@@ -1,6 +1,8 @@
 class PotepanController < ApplicationController
 
   def index
+    @new_arrivals = Spree::Product.order('id DESC').limit(5)
+    @new_arrival_images = @new_arrivals.map{ |prd| prd.display_image.attachment(:large) }
   end
 
 end
