556efbb16f3298e9f01cb8762e4c00fb94c8ab15
Koji Onishi <fursich0@gmail.com>
2017-05-01 00:11:14 +0900

    価格表示を修正･統一化(国際化対応や税金計算の汎用性は未対応)

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -9,7 +9,7 @@ class Potepan::ProductsController < ApplicationController
 
   def index
     if params[:category]
-      @products = Spree::Product.with_taxons_name( params[:category] ).includes(:prices)
+      @products = Spree::Product.with_taxons_name( params[:category] )
     else
       @products = Spree::Product.all
     end
